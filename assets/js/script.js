ed0384a6050099fb0d6c4f4fdc29d4df272599d5
Stanghi <stangherlin.matti@gmail.com>
2022-10-26 18:14:13 +0200

    [add] pilotActive

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,11 +34,17 @@ const f1pilot = [
 let counterImgs = 0;
 let numImgs = f1pilot.length;
 
+const slider = document.querySelector('.slider');
 const textOutput = document.querySelector('.info-pilot');
 const pilotBig = document.querySelector('.pilot-big');
+const pilotActive = document.getElementsByClassName('pilot');
 
 const prev = document.querySelector('.btn-left');
 const next = document.querySelector('.btn-right');
+
+pilotActive[counterImgs].classList.add('pilot-active');
+
+autoCarousel();
 
 for (indice in f1pilot){
 
@@ -46,24 +52,30 @@ for (indice in f1pilot){
     next.addEventListener('click', nextFunction);
 
     const pilot = f1pilot[counterImgs];
+
     output(pilot);
 }
 
 function prevFunction(){
+    pilotActive[counterImgs].classList.remove('pilot-active');
     counterImgs--;
     if(counterImgs < 0) counterImgs = numImgs - 1;
     const pilot = f1pilot[counterImgs];
     output(pilot);
+    pilotActive[counterImgs].classList.add('pilot-active');
 }
 
 function nextFunction(){
+    pilotActive[counterImgs].classList.remove('pilot-active');
     counterImgs++;
     if(counterImgs === numImgs) counterImgs = 0;
     const pilot = f1pilot[counterImgs];
     output(pilot);
+    pilotActive[counterImgs].classList.add('pilot-active');
 }
 
 function output(pilot){
+    console.log(pilot);
     textOutput.innerHTML = `
     <h2>${pilot.name} ${pilot.lastname}</h2>
     <p>Scuderia: ${pilot.team}</p>`;
@@ -72,13 +84,12 @@ function output(pilot){
     <img src="assets/img/${pilot.img}" alt="${pilot.lastname}">`;
 }
 
+function autoCarousel(){
+    autoPlay = setInterval(nextFunction, 1250);
+}
 
+slider.addEventListener('mouseover', function(){
+    clearInterval(autoPlay);
+});
 
-
-
-
-
-
-
-
-
+slider.addEventListener('mouseout', autoCarousel);
